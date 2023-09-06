e7896880253aeb4855d9869858b996db34983263
Cyril Jouve <jv.cyril@gmail.com>
2023-09-06 22:16:25 +0200

    subtree helper

---
+++ git-subtree-pull.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -eux -o pipefail
+
+git_subtree_pull () {
+    src_url=$1
+    src_tag=$2
+    src_dir=$3
+    tgt_dir=$4
+    tmp=$(mktemp -d)
+    git clone "$src_url" "$tmp"
+    ref=$(git -C "$tmp" subtree split "--prefix=$src_dir" "$src_tag")
+    git subtree pull "--prefix=$tgt_dir" "$tmp" "$ref"
+}
+
+git_subtree_pull https://github.com/cloudnative-pg/charts cloudnative-pg-v0.18.2 charts/cloudnative-pg charts/cloudnative-pg
+git_subtree_pull https://github.com/coredns/helm          coredns-1.26.0         charts/coredns charts/coredns
