144e4e32ebed2591a5fc8afa85140f8be82e05c6
Cyril Jouve <jv.cyril@gmail.com>
2023-09-27 12:05:21 +0200

    [gatekeeper-library] bd942ad8afd316743fbe97a69c5019d349c1a5c1

--- charts/gatekeeper-library/templates.sh
+++ charts/gatekeeper-library/templates.sh
@@ -4,3 +4,4 @@ set -eux -o pipefail
 rm -rf templates
 mkdir templates
 kustomize build | yq --no-doc --split-exp '.metadata.name | sub(".*", "templates/${0}.yaml")'
+yq -i '.appVersion = (load("kustomization.yaml").resources.0 | sub(".*ref=(.*)", "$1"))' Chart.yaml
