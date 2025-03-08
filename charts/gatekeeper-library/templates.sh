b9da71d03d673f8aebfed657125a6fd47aa171e9
Cyril Jouve <jv.cyril@gmail.com>
2025-03-08 16:15:58 +0100

    bump images

--- charts/gatekeeper-library/templates.sh
+++ charts/gatekeeper-library/templates.sh
@@ -3,5 +3,7 @@
 set -eux -o pipefail
 rm -rf templates
 mkdir templates
+# shellcheck disable=SC2016
 kustomize build | yq --no-doc --split-exp '.metadata.name | sub(".*", "templates/${0}.yaml")'
+# shellcheck disable=SC2016
 yq -i '.appVersion = (load("kustomization.yaml").resources.0 | sub(".*ref=(.*)", "$1"))' Chart.yaml
